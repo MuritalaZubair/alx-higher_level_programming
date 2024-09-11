27b1ba4b4f6ce59752f248f47ac64ec68b267ba0
Ola <47708292+MuritalaZubair@users.noreply.github.com>
2024-09-12 00:33:17 +0100

    Update 101-revove_char_at.py

--- 0x01-python-if_else_loops_functions/101-revove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-revove_char_at.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-def remove_char_at(s, n):
+
+def remove_char_at(str, n):
     if n < 0:
-        return s
-    return s[:n] + s[n + 1:]
+        return (str)
+    return str[:n] + str[n + 1:]
