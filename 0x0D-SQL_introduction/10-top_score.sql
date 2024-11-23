5104742d8841e4c0ad8c9791016f71ef773ddb63
MuritalaZubair <olaniyizubair@gmail.com>
2024-11-22 21:25:26 -0800

    Introduction

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- lists all rows of a table second_table
+-- from the database hbtn_0c_0
+-- results display score and name
+-- ordered by score
+SELECT score, name FROM second_table ORDER BY score DESC;
---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,3 @@
+-- removes all records where the score is <= 5 from
+-- second table
+DELETE FROM second_table WHERE score <= 5;
